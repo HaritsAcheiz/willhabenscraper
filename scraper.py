6e1becd0233cebf649f49b9b388cca3af285574f
HaritsAcheiz <harits.muhammad.only@gmail.com>
2023-03-14 04:09:14 +0700

    parsing complete, next to the api

--- scraper.py
+++ scraper.py
@@ -1,26 +1,34 @@
-import httpx
 import aiohttp
 from selectolax.parser import HTMLParser
-from dataclasses import dataclass
+from dataclasses import dataclass, asdict
 import asyncio
 import random
 from time import perf_counter
 from typing import List
 
-@data
+@dataclass
+class Product:
+        name: str
+        img: str
+        link: str
+        price: str
+        size: str
 
 @dataclass
 class Scraper:
-        proxies : List[str]
+        proxies: List[str]
+        useragent: List[str]
+        baseurl: str = 'https://www.willhaben.at'
 
         async def fetch(self, s, url):
+                ua = random.choice(self.useragent)
                 headers = {
-                        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:109.0) Gecko/20100101 Firefox/110.0',
+                        'User-Agent': ua,
                         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
                         'Accept-Language': 'en-US,en;q=0.5',
                         'Accept-Encoding': 'gzip, deflate, br',
+                        'Cookie': 'bbxDmpSegments=[]; bbxLastViewed={"bap":{"adList":[656862016]}}; IADVISITOR=afb2210f-26b8-4223-b378-03fef213e347; context=prod; TRACKINGID=7eb82009-ab62-4fef-b62a-1d93017e2c5e; x-bbx-csrf-token=5f2567cf-990a-4b7a-834f-d15bb3d1dd02; SRV=3|ZA+In; atuserid=%7B%22name%22%3A%22atuserid%22%2C%22val%22%3A%22c118a7be-8680-469a-bd2d-b3b051cd9014%22%2C%22options%22%3A%7B%22end%22%3A%222024-04-13T20%3A17%3A37.455Z%22%2C%22path%22%3A%22%2F%22%7D%7D; atidvisitor=%7B%22name%22%3A%22atidvisitor%22%2C%22val%22%3A%7B%22vrn%22%3A%22-612451-%22%7D%2C%22options%22%3A%7B%22path%22%3A%22%2F%22%2C%22session%22%3A31536000%2C%22end%22%3A31536000%7D%7D; ioam2018=00010a06df4d83560640f7902:1708370818778:1678735618778:.willhaben.at:16:at_w_atwillhab:Service/Rubrikenmaerkte/Sonstiges/moewa/Kameras_TV_Multimedia/TL:noevent:1678739853806:fhwjlr; didomi_token=eyJ1c2VyX2lkIjoiMTg2ZGM3MGItMzkwNS02YmRkLThkYmEtNDg0ZmIzNjUwYmJmIiwiY3JlYXRlZCI6IjIwMjMtMDMtMTNUMTk6Mjc6MDIuMjc3WiIsInVwZGF0ZWQiOiIyMDIzLTAzLTEzVDE5OjI3OjAyLjI3N1oiLCJ2ZW5kb3JzIjp7ImVuYWJsZWQiOlsiYW1hem9uIiwiZ29vZ2xlIiwiYzpvZXdhLVhBUW1HU2duIiwiYzphbWF6b24tbW9iaWxlLWFkcyIsImM6aG90amFyIiwiYzp1c2Vyem9vbSIsImM6YW1hem9uLWFzc29jaWF0ZXMiLCJjOnh4eGx1dHprLW05ZlFrUHRMIiwiYzpvcHRpb25hbGUtYm5BRXlaeHkiXX0sInB1cnBvc2VzIjp7ImVuYWJsZWQiOlsieHh4bHV0enItcWtyYXAzM1EiLCJnZW9sb2NhdGlvbl9kYXRhIiwiZGV2aWNlX2NoYXJhY3RlcmlzdGljcyJdfSwidmVuZG9yc19saSI6eyJlbmFibGVkIjpbImdvb2dsZSIsImM6d2lsbGhhYmVuLVpxR242WXh6Il19LCJ2ZXJzaW9uIjoyLCJhYyI6IkFrdUFFQUZrQkpZQS5Ba3VBRUFGa0JKWUEifQ==; RANDOM_USER_GROUP_COOKIE_NAME=29; _pbjs_userid_consent_data=7882712499466548; _pulse2data=bb9fb85e-7a09-4052-8f45-0b01c9ffc8c6%2Cv%2C%2C1678740279140%2CeyJpc3N1ZWRBdCI6IjIwMjMtMDMtMTNUMTk6Mjc6MDBaIiwiZW5jIjoiQTEyOENCQy1IUzI1NiIsImFsZyI6ImRpciIsImtpZCI6IjIifQ..KAIfPyO9V0C5GWTXiyV9-g.Jel9xNaPsjEC8w2z1zcv0BHIUQWqtv9KU6VtjJ08Ei4NIw8ggJN2WjwJzqhi8Uq82TertthSmd4v6cRe55Ev8mSfSZiEMxxjI4fQ4XRpVxKFs-n93EErSoU6IDYgKloQ6wXI2LqixrB3WRhc-ZyK6H0xJBWIuTc2FPUOWNFuT6fjditiHy7TUT71uXfJ0BTxyFjNeDWVoDBqwULD2YxPYQ.Zo8WuCEhIyEg6EsZx7ANqA%2C%2C%2Ctrue%2C%2CeyJraWQiOiIyIiwiYWxnIjoiSFMyNTYifQ..d3CcqTFN1s8nYMOpAmFOkBjkGV4VRnaZ8RaJh6NNG3s; euconsent-v2=CPokAsAPokAsAAHABBENC7CsAP_AAH_AAAAAIzNf_X_fb2_j-_59f_t0eY1P9_7_v-0zjhedk-8Nyd_X_L8X52M7vB36pq4KuR4ku3LBAQdlHOHcTQmw6IkVqSPsbk2Mr7NKJ7PEmlMbO2dYGH9_n1XT-ZKY79__f_7z_v-v____7r__7-3f3_vp9V-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABA3QAkw1biALsSxwJtowigRAjCsJCoBQAUUAwtEBhASuCnZXAT6wiQAIBQBGBECHAFGDAIAAAIAkIiAECPBAAACIBAACABUIhAARoAgoAJAQCAAUA0LACKAIRJCDIiIilMCAiRIKCeQIQSg_0MMIQ6igAAA.f_gAD_gAAAAA; permutive-id=352d3fa4-e48e-4ccf-8015-789bf2773800; _lr_env_src_ats=false; id5id.1st_426_nb=2; pbjs-unifiedid=%7B%22TDID%22%3A%229cfe700f-37dd-44b6-8b2c-ab8d0da8fa3f%22%2C%22TDID_LOOKUP%22%3A%22FALSE%22%2C%22TDID_CREATED_AT%22%3A%222023-03-13T19%3A27%3A03%22%7D; cto_bundle=V6Il9F9yUTAlMkYzeUJNcW43UUZBMFVqN2FuRWdWSWtjRUtLZlVDbXU2MW80eFVXWCUyQlhkNnhacWc3OThFRUlEblclMkJMSElmN0UweHo5VWd3dW5yVEZ6Y2N1QWIzbTZTVTRteFgzelNjWVUwTjJ4U1ZqJTJGV1dZM3Y4Vmp5JTJGaHpYRTE1NE5PR2hndHl6YjJFZEloaXhCNiUyRnFJZG1CMEElM0QlM0Q; cto_bidid=xFKRoV9YYkVuajNiVnFpY25RcEwlMkZ6VkRnWG91WldKR1VkMVpqTHhldkp3djBqcjlaYWlJUzl1ZEo0SFkxcjB0RyUyRkREZGNBalElMkI3QUR5WVBPREhVQzMlMkZ0SjElMkJydnYzRFBwZmd5RmYzVVF0TEJ2S28lM0Q; IS_FIRST_AD_REQUEST_V2=false; COUNTER_FOR_ADVERTISING_FIRST_PARTY_UID_V2=9; ADVERTISING_FIRST_PARTY_UID_V2=dfd12a9b-b424-492e-af85-b64e1068cb03; __gads=ID=25f53b83fe55ef4a:T=1678735706:S=ALNI_MaS7FcrQfuFPeguTs4rHCZLYTe2lw; __gpi=UID=00000bd8b59e7a73:T=1678735706:RT=1678735706:S=ALNI_MY-PMvNXW5WjW7FPx6P49huhQfN1g; CONSENT_FOR_FIRST_PARTY_COOKIES=true; FIRST_PARTY_TRACKINGID=7eb82009-ab62-4fef-b62a-1d93017e2c5e',
                         'Connection': 'keep-alive',
-                        # 'Cookie': 'bbxDmpSegments=[]; SRV=5|ZA4wu; IADVISITOR=9742ab70-deb0-43c4-9b29-7ca8553a937e; context=prod; TRACKINGID=abd7e221-9f47-4d91-a067-234ea882a501; x-bbx-csrf-token=ef17beb6-f847-4d83-8430-6f2e63c478fc; atuserid=%7B%22name%22%3A%22atuserid%22%2C%22val%22%3A%22f0db7e6e-e50a-4c77-b292-16d1ea4cad53%22%2C%22options%22%3A%7B%22end%22%3A%222024-04-12T20%3A00%3A26.553Z%22%2C%22path%22%3A%22%2F%22%7D%7D; atidvisitor=%7B%22name%22%3A%22atidvisitor%22%2C%22val%22%3A%7B%22vrn%22%3A%22-612451-%22%7D%2C%22options%22%3A%7B%22path%22%3A%22%2F%22%2C%22session%22%3A31536000%2C%22end%22%3A31536000%7D%7D; ioam2018=00022df0f26227ab6640e2e5f:1705866976371:1678650976371:.willhaben.at:7:at_w_atwillhab:Service/Rubrikenmaerkte/Sonstiges/Mode_Accessoires/TL:noevent:1678651575434:159dew; didomi_token=eyJ1c2VyX2lkIjoiMTg2ZDc2NTItOWY0ZS02NjYxLTg0YTktNTcxZjFhOTgzMjFiIiwiY3JlYXRlZCI6IjIwMjMtMDMtMTJUMTk6NTY6MjMuNDQ1WiIsInVwZGF0ZWQiOiIyMDIzLTAzLTEyVDE5OjU2OjIzLjQ0NVoiLCJ2ZW5kb3JzIjp7ImVuYWJsZWQiOlsiYW1hem9uIiwiZ29vZ2xlIiwiYzpvZXdhLVhBUW1HU2duIiwiYzphbWF6b24tbW9iaWxlLWFkcyIsImM6aG90amFyIiwiYzp1c2Vyem9vbSIsImM6YW1hem9uLWFzc29jaWF0ZXMiLCJjOnh4eGx1dHprLW05ZlFrUHRMIiwiYzpvcHRpb25hbGUtYm5BRXlaeHkiXX0sInB1cnBvc2VzIjp7ImVuYWJsZWQiOlsieHh4bHV0enItcWtyYXAzM1EiLCJnZW9sb2NhdGlvbl9kYXRhIiwiZGV2aWNlX2NoYXJhY3RlcmlzdGljcyJdfSwidmVuZG9yc19saSI6eyJlbmFibGVkIjpbImdvb2dsZSIsImM6d2lsbGhhYmVuLVpxR242WXh6Il19LCJ2ZXJzaW9uIjoyLCJhYyI6IkFrdUFFQUZrQkpZQS5Ba3VBRUFGa0JKWUEifQ==; _pulse2data=590120fd-088a-41f1-9877-3794cd3e37c1%2Cv%2C%2C1678651877727%2CeyJpc3N1ZWRBdCI6IjIwMjMtMDMtMTJUMTk6NTY6MTZaIiwiZW5jIjoiQTEyOENCQy1IUzI1NiIsImFsZyI6ImRpciIsImtpZCI6IjIifQ..3qH9DDYdf8XEoalGIsayMQ.DXV9o-oxncPxypbrpMN57xmbBD7axrMq7CdXF0fSZSNoRBlDkdlJjI0rRUlQD7EhXEAbS_VR0PlYDlC1gFi3BT0wLdR_nZxU1XEVnuxCyCG4ERBLLeutJCYwLX-gOgLPqQLrcEzzpB5Km0x7NqEhz6cjzr0U1VA2qlgVdosWJLaB_vFdjKkWBcauSUGHoOY9Ek6zAZn7DxOUip-zJmETkA.7ZcCDT_oj9_fWLmJiFQhLA%2C%2C%2Ctrue%2C%2CeyJraWQiOiIyIiwiYWxnIjoiSFMyNTYifQ..rBb2C2s0jzOwzy6OEdqdvbCKJ0wrQs7749UxPVnBmP4; RANDOM_USER_GROUP_COOKIE_NAME=22; _pbjs_userid_consent_data=5425172714880651; _lr_retry_request=true; _lr_env_src_ats=false; pbjs-unifiedid=%7B%22TDID%22%3A%223c46eca8-6efd-424a-8582-4511c31870d9%22%2C%22TDID_LOOKUP%22%3A%22FALSE%22%2C%22TDID_CREATED_AT%22%3A%222023-03-12T19%3A56%3A20%22%7D; cto_bundle=brsLzV93dXVuYTd0dVBJRmgzSGdwJTJCaENTSzFtUCUyQm1HOFV5RGZyZkVRa0ZPbnh4YnlLSmtHWXVxTWFMJTJCNSUyQmhWcHpCeGhHbGRyZDVJM1l5ODhrTmh2b2x2TFBtaUdGZTg4c1o1YklKck1WZEVUUDlVJTJCclNiakJXVG4wamJ4UkV3RWNMMXNTOXdWdlVCQnN5bmowdnNDVGplSTFBJTNEJTNE; cto_bidid=WK37WF9PQ045VlV3VDE1YmJsdjdEUkUxRUdZQThBdmFCb3Rxazg3YkNYc2wlMkY5c3BKdjRpVWdpWVhCSWdRR0FTYmpTNCUyQldBZUh3aDYlMkJpSGNSNmJMSjVScWZKT3lCTDZJNkFaMXZXU1VTN01pOUpXdyUzRA; euconsent-v2=CPogtwAPogtwAAHABBENC6CsAP_AAH_AAAAAIzNf_X_fb2_j-_59f_t0eY1P9_7_v-0zjhedk-8Nyd_X_L8X52M7vB36pq4KuR4ku3LBAQdlHOHcTQmw6IkVqSPsbk2Mr7NKJ7PEmlMbO2dYGH9_n1XT-ZKY79__f_7z_v-v____7r__7-3f3_vp9V-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABA3QAkw1biALsSxwJtowigRAjCsJCoBQAUUAwtEBhASuCnZXAT6wiQAIBQBGBECHAFGDAIAAAIAkIiAECPBAAACIBAACABUIhAARoAgoAJAQCAAUA0LACKAIQJCDIgIilMCAiRIKCeQIQSg_0MMIQ6igAAA.f_gAD_gAAAAA; permutive-id=e1fb75fb-33e4-48da-b804-f0ae850e0882; id5id.1st_426_nb=4; IS_FIRST_AD_REQUEST_V2=false; COUNTER_FOR_ADVERTISING_FIRST_PARTY_UID_V2=6; __gads=ID=9642433c3eacd087:T=1678651046:S=ALNI_Mb9nNf9Ie6J-bujyAbjDBbjRMFwIg; __gpi=UID=00000bd862a85073:T=1678651046:RT=1678651046:S=ALNI_MZj3cYD2iRVy7WNzaN8D4itttRjNw; CONSENT_FOR_FIRST_PARTY_COOKIES=true; ADVERTISING_FIRST_PARTY_UID_V2=d77c5f97-8ae2-40db-9a88-a72620abf047; FIRST_PARTY_TRACKINGID=abd7e221-9f47-4d91-a067-234ea882a501; id5id.1st=%20%7B%20%22created_at%22%3A%20%222023-03-12T20%3A00%3A07.531478818Z%22%2C%20%22id5_consent%22%3A%20true%2C%20%22original_uid%22%3A%20%22ID5*OQOCQVvuyJVsBVpZZAiWkU0ygyj1O8QUrfeKSOj-L_o-HLrW7x-IZb2sPEyePSVWPh5s93X0O7L-nKFAqBWUeD4f3HzQYy2unxtqg8g8Qp8-IPZztK-xwMLTo0pgks8MPiHYO7tjhmwwYl6dQn8-UT4iEujf0QjFNwQVJGRRLTU-J3_c3ZeJ-OE_nyth3fxKPiix0T6CuRcq4bCal6piPT4pCNLw3H5rqtCANqrro-g-KopFWf6w6RYAYp4TwZUdPiwEFoX2Qk32A5G-DSZE3T4upQOIb5YQOSz8zbti_Bc-Lz0OTtlTSR6-IHvJSVNvPjBy83UvYr6XDtUXrpJ1Vz4ytDYbSSfKFjw3gUbaCxg-MwGlB5ZkA7UaMi8o54HnPjYwWkWlbVXN-qhQbRusMz43H5oSHdiLEpC8aAm9_cE-OZ7y508v0G6KPuHeAARWPjyatFU-N8jwrA3CvaWB4j49GS-KBNZGDlDV5mrr_go-P_wmS4t1gZ0RxYxIyPwePkIu4cjEZv9AdzdgINdYYz5DU350oy0Hrw2p8pp25DM-Rxo9IfZUiwmVE_y79D5J%22%2C%20%22universal_uid%22%3A%20%22ID5*l3scth2-IY0sooZx65sZWMOE_GSdoHWYLpnv5hK3VwQ-HNT3co58WUQGoNqZQ-gOPh6gvRVSDjVltBop7KBfaD4fuxAMlXOLCt27PFWD5OU-IEx05jypwTeDgsflWGWVPiEYg7Krd2JTRLjRiwkYpj4ileGdx6UQkL1NvQejwFM-J508mjfrMPjjqyXEbXAqPig55zsrJDZj0GaiZSeE0D4p7qA5pozWeVQO0qIva2s-KscDjWfQA4CVz_QzjLmLPixzaHvQoufBfOEw_cijEz4uspPBrk9nPHIxE3kWcoY-LwWkKzbxsg2Tg5iqD47UPjCzm1qzgKItcFlmFUktIj4yEt0r_iNDX6Vw1XtMy14-M0Axbm2-vCmGxUUSJ6RnPjYGimxCeDOPwznapx2_Pz43DjNmQTEpWVFvYKeIS7c-OXw2BVitWegmuFt7KIspPjwJkTVR6ZNHcN7FBNEceT49Qa30-rrwfS1yaf1ob4U-Py9LYrdm9mBr6MNcgL6jPkK3G0r-rXn1e2zwbej-Cj5Doyl0LD7og4fKjn2fujE-R9IttFfaKkOJOIT7Dboh%22%2C%20%22signature%22%3A%20%22ID5_AhD4p0SQK3Z9ncM2bDecpzIq3TK3g4dvJppOdDNnJVkxQLkwpdXsgslI9EoEsPzznhQqNL62_Vd5JCHd3a3_57fB9H2v%22%2C%20%22link_type%22%3A%201%2C%20%22cascade_needed%22%3A%20true%2C%20%22privacy%22%3A%20%7B%20%22jurisdiction%22%3A%20%22gdpr%22%2C%20%22id5_consent%22%3A%20true%7D%7D; id5id.1st_last=1678651208498',
                         'Upgrade-Insecure-Requests': '1',
                         'Sec-Fetch-Dest': 'document',
                         'Sec-Fetch-Mode': 'navigate',
@@ -46,16 +54,33 @@ class Scraper:
                 res = await asyncio.gather(*tasks)
                 return res
 
-        async def main(self):
+        async def main(self, keyword):
                 urls = list()
                 for page in range(1,20):
-                        url = f'https://www.willhaben.at/iad/kaufen-und-verkaufen/marktplatz/mode-accessoires-3275/a/zustand-neu-22?keyword=sneaker&sfId=cfccbeaa-2f0b-49ca-881f-ec28c6b42f7f&isNavigation=true&page={page}&rows=90'
+                        url = f'https://www.willhaben.at/iad/kaufen-und-verkaufen/marktplatz?sfId=8ef62b18-a054-404c-8f50-f28cd3ce1a00&isNavigation=true&keyword={keyword}&rows=90&page={page}'
                         urls.append(url)
                 print(urls)
 
                 async with aiohttp.ClientSession() as s:
                         htmls = await self.fetch_all(s, urls)
-                print(htmls)
+                return htmls
+
+        def parser(self, htmls):
+                for html in htmls:
+                        tree = HTMLParser(html)
+                        stage1 = tree.css('#skip-to-resultlist > div.jYFQjC')
+                        for item in stage1:
+                                try:
+                                        new_item = Product(
+                                                name = item.css_first('h3').text(),
+                                                img = item.css_first('img[alt="Cover Image"]').attributes['src'],
+                                                link = self.baseurl + item.css_first('a').attributes['href'],
+                                                price = item.css_first('div:nth-child(2) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > span:nth-child(1)').text(),
+                                                size = item.css_first('div:nth-child(2) > div:nth-child(2) > div:nth-child(3)').text()
+                                        )
+                                except:
+                                        continue
+                                print(asdict(new_item))
 
 
 if __name__ == '__main__':
@@ -71,8 +96,19 @@ if __name__ == '__main__':
                 '196.51.114.248:8800',
                 '142.214.183.70:8800'
         ]
+
+        useragent = [
+                'Mozilla/5.0 (Wayland; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.137 Safari/537.36 Ubuntu/22.04 (5.0.2497.35-1) Vivaldi/5.0.2497.35',
+                'Mozilla/5.0 (Wayland; Linux x86_64; System76 Galago Pro (galp2)) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.175 Safari/537.36 Ubuntu/22.04 (5.0.2497.48-1) Vivaldi/5.0.2497.48',
+                'Mozilla/5.0 (Wayland; Linux x86_64; System76 Galago Pro (galp2)) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.175 Safari/537.36 Ubuntu/22.04 (5.0.2497.51-1) Vivaldi/5.0.2497.51,',
+                'Mozilla/5.0 (Wayland; Linux x86_64; System76) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.133 Safari/537.36 Ubuntu/22.04 (5.2.2623.34-1) Vivaldi/5.2.2623.39',
+                'Mozilla/5.0 (Wayland; Linux x86_64; System76) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.92 Safari/537.36 Ubuntu/22.04 (5.2.2623.34-1) Vivaldi/5.2.2623.34'
+        ]
+
+        keyword = 'kamera'
         start = perf_counter()
-        s = Scraper(proxies=proxies)
-        asyncio.run(s.main())
+        s = Scraper(proxies=proxies, useragent=useragent)
+        htmls = asyncio.run(s.main(keyword=keyword))
+        s.parser(htmls)
         stop = perf_counter()
         print(f'time taken: {stop - start}')
